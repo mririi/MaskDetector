4586722f77f17c38942d82a7edd458e92e1f4395
wassim.mriri@gmail.com <wassim.mriri@gmail.com>
2024-04-23 21:58:43 +0100

    fixed test issue

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,10 +3,9 @@ from app import app
 
 @pytest.fixture
 def client():
-    app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
 
-def test_get(client):
+def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
